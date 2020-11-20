df39974dfbf82f7ca17c84bfba9b466ce4601ce5
estein1988 <estein1988@gmail.com>
2020-11-20 10:56:13 -0700

    Add alternative solutions to reverse a string.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -10,4 +10,19 @@ function reverse(str) {
     return str.split('').reverse().join('')
 }
 
+function reverse(str) {
+    let reversed = ''
+
+    for (let character of str){
+        reversed = character + reversed
+    }
+    return reversed
+}
+
+function reverse(str){
+    return str.split('').reduce((reversed, character) => {
+        return character + reversed
+    }, '');
+}
+
 module.exports = reverse;
